4a353e4647a15d71cf03c8738707a7b782eac341
Abijith12345 <125263815+Abijith12345@users.noreply.github.com>
2023-11-16 15:19:54 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = var.ami
+  instance_type = var.instance_type
+ 
+  tags = {
+    Name = "test2"
+  }
+}
 
